b4892c44a00e647bc243c1f5a14e33e442b23ba1
Manasadepa <manasadepa9@gmail.com>
2025-04-29 13:21:41 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,35 @@
+print("welcome on board for playing the quiz")
+print('the top one would be awarded with 50 lakhs of "cashprize"')
+question1=("1) who is the chief minister of Telangana")
+options=('A.KCR B.KTR C.Revanth Reddy D.chandra babu')
+print(question1)
+print(options)
+ans=input('choose the correct answer from given options:')
+if (ans=='C'):
+    print("congrajulations you have won cashprize of 10 lakhs")
+else:
+    print('Wrong answer! better luck next time')
+
+question2 = ("2) Square root of 15")
+options2= ('A.1500 B.25 C.255 D.225')
+print(question2)
+print(options2)
+ans = input('choose the correct answer from given options:')
+if (ans == 'D'):
+        print("congrajulations you have won cashprize of 15 lakhs")
+        print('final amount received:25lakhs')
+else:
+        print('Wrong answer! better luck next time')
+        print('final amount received:10lakhs')
+
+question2 = ("3) what is the state bird of telangana")
+options2= ('A.Indian roller(pala pitta) B.parrot C.peacock D.sparrow')
+print(question2)
+print(options2)
+ans = input('choose the correct answer from given options:')
+if (ans == 'A'):
+        print("congrajulations you have won cashprize of total 25lakhs")
+        print('final amount received:50lakhs')
+else:
+        print('Wrong answer! better luck next time')
+        print('final amount received:25lakhs')
